c29d634b68d3c85f57c6618723663d8d1f9e0722
Enes Can Aydemir <enescan_aydemir@hotmail.com>
2022-04-10 17:25:37 +0300

    modified

--- test.js
+++ test.js
@@ -19,18 +19,18 @@ const Photo = mongoose.model('Photo', photoSchema)
 
 
 //create a photo (photo içinde dataları oluşturuyoruz.)
-/* Photo.create({
+Photo.create({
     title: "Photo Title 2",
     description: "Photo description 2 lorem ipsum",
-}) */
+})
 
 //read a photo (verileri okuma)
-/* Photo.find({}, (err, data) => { //callback fonksiyonu içierisine 2 parametre. err ve data. datayı console için çağırdık.
+Photo.find({}, (err, data) => { //callback fonksiyonu içierisine 2 parametre. err ve data. datayı console için çağırdık.
     console.log(data);
-})*/
+})
 
 //update a photo
-/*const id = "6252a4e3e43b2759505f3aec";
+const id = "6252a4e3e43b2759505f3aec";
 
 Photo.findByIdAndUpdate(
     id, { // 1.parametre olarak id aldık, 2. parametre olarak yapmak istediğimiz değişiklikleri yazdık
@@ -42,7 +42,7 @@ Photo.findByIdAndUpdate(
     (err, data) => { //callback function
         console.log(data)
     }
-)*/
+)
 
 // delete a photo
 const id = '6252a627a73de99f218bbfb5';
