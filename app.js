3cf598547b9248f8ab40178396d861ac5714b337
Enes Can Aydemir <enescan_aydemir@hotmail.com>
2022-04-11 11:55:56 +0300

    Add Photo sayfasında add.post ile  upload işlemi

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ app.set("view engine", "ejs");
 
 // MIDDLEWARES
 app.use(express.static('public'))
+    // Aşağıda yazdığımız middlewareler req,res döngüsünde aldığımız requesti sonlandırmamıza yardımcı oldu.
+app.use(express.urlencoded({ extended: true })) // urlencoded = url'deki datayı okumamızı sağlıyor
+app.use(express.json()) // url deki
 
 
 // ROUTES
@@ -21,6 +24,11 @@ app.get('/about', (req, res) => {
 })
 app.get('/add', (req, res) => {
     res.render('add')
+})
+
+app.post('/photos', (req, res) => {
+    console.log(req.body);
+    res.redirect('/') // console'a yazdıktan sonra ana sayfaya dönmesini söyledik(index e redirect etmek)
 })
 
 const port = 3000;
