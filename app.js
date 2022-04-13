c1475dbec93e3e4c94ac612bcc47951b3e4bae5e
Enes Can Aydemir <enescan_aydemir@hotmail.com>
2022-04-13 16:27:04 +0300

    Delete Photo

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const fileUpload = require('express-fileupload')
+const methodOverride = require('method-override')
 
 const ejs = require('ejs')
 const fs = require('fs')
@@ -24,6 +25,7 @@ app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true })); // urlencoded = url'deki datayı okumamızı sağlıyor
 app.use(express.json()) // url deki datayı JSON formatına döndürmemizi sağlıyor.
 app.use(fileUpload())
+app.use(methodOverride('_method'))
 
 // ROUTES
 app.get('/', async(req, res) => {
@@ -34,8 +36,6 @@ app.get('/', async(req, res) => {
 })
 
 app.get('/photos/:id', async(req, res) => {
-    //console.log(req.params.id)
-    //res.render('about')
     const photo = await Photo.findById(req.params.id)
     res.render('photo', {
         photo,
@@ -50,9 +50,6 @@ app.get('/add', (req, res) => {
 })
 
 app.post('/photos', async(req, res) => {
-    //console.log(req.files.image)
-    // await Photo.create(req.body)
-    // res.redirect('/')
 
     const uploadDir = 'public/uploads'
 
@@ -70,6 +67,22 @@ app.post('/photos', async(req, res) => {
         })
         res.redirect('/')
     })
+})
+
+app.get('/photos/edit/:id', async(req, res) => {
+    const photo = await Photo.findOne({ _id: req.params.id })
+    res.render('edit', {
+        photo
+    })
+})
+
+app.put('/photos/:id', async(req, res) => {
+    const photo = await Photo.findOne({ _id: req.params.id })
+    photo.title = req.body.title
+    photo.description = req.body.description
+    photo.save()
+
+    res.redirect(`/photos/${req.params.id}`)
 })
 
 const port = 3000;
